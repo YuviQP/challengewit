562b5e2a3e0f6a39725f619555b8c69d98a132be
YuviQP <yuvisa.palomino@gmail.com>
2024-07-27 10:54:23 -0300

    created files and folder for use in the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { 
+  BrowserRouter as Router,
+  Routes, 
+  Route 
+} from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+
+
+import Home from './pages/home/home';
+import themes from './theme/themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themes}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/theme/themes.js
@@ -0,0 +1,28 @@
+import { createTheme } from "@mui/material/styles";
+
+const themes = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#6750A4",
+    },
+    secondary: {
+      main: "#625B71",
+    },
+    tertiary: {
+      main: "#7D5260",
+    },
+    error: {
+      main: "#B3261E",
+    },
+    background: {
+      default: "#ffffff",
+    },
+    typography: {
+      fontFamily: "Roboto, Arial, sans-serif",
+    },
+  },
+
+});
+
+export default themes;
